c0a88a0496c4f1f98d8c930fe75d322459541f17
Waqar Rashid <waqarrashid33@gmail.com>
2022-06-03 16:26:49 +0200

    Blinkding Led

---
+++ toggle_gpio.py
@@ -0,0 +1,16 @@
+import time
+
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(5, GPIO.OUT)
+
+while True:
+    GPIO.output(5, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(5, GPIO.LOW)
+    time.sleep(0.5)
