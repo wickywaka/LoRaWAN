bdcd05d56ee891061e908213a9d698cf587a0aa8
Waqar Rashid <waqarrashid33@gmail.com>
2022-06-03 16:40:32 +0200

    Improved cleanup

--- toggle_gpio.py
+++ toggle_gpio.py
@@ -5,14 +5,19 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
-GPIO.cleanup()
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(5, GPIO.OUT)
+try:
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(5, GPIO.OUT)
 
-while True:
-    print("ON")
-    GPIO.output(5, GPIO.HIGH)
-    time.sleep(0.5)
-    print("OFF")
-    GPIO.output(5, GPIO.LOW)
-    time.sleep(0.5)
+    while True:
+        print("ON")
+        GPIO.output(5, GPIO.HIGH)
+        time.sleep(0.5)
+        print("OFF")
+        GPIO.output(5, GPIO.LOW)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    print("Exiting..")
+finally:
+    GPIO.cleanup()
+    print("Cleanup Done")
